4ba7a57a9c36901457cffa20702a9afc5f9b60e0
traditionallimb <dom@kalowe.me.uk>
2022-12-30 13:26:04 +0000

    edit: removed a bunch of files

--- Python/202X Python/gameTrackr/steamAchievGet.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=440&key=406499F72B871BA02972C93877D5B744&steamid=76561198436661503"
-steamKey = "406499F72B871BA02972C93877D5B744"
-
-response = requests.request("GET", url, params="apiname")
-
-print(response.text)
--- Python/202X Python/revision test.py
+++
@@ -1,9 +0,0 @@
-counter = 0
-for counter in range(1,101):
-    if counter%3:
-        print("Fizz")
-    elif counter%5:
-        print("Buzz")
-    else:
-        print(counter)
-    counter += 1

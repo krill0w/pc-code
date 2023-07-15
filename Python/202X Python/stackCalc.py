3c24c0e8fff293ac6ebb17d5a5ef294e79e69ea0
traditionallimb <dom@kalowe.me.uk>
2023-07-15 11:46:41 +0000

    mk8dr new wave tracks update

--- Python/202X Python/stackCalc.py
+++ Python/202X Python/stackCalc.py
@@ -1,10 +1,10 @@
 stackOption = int(input("Would you like to convert:\n\t1. Stack to Blocks\n\t2. Blocks to Stack\nEnter a number\n> "))
 if stackOption == 1:
-  numToConv = int(input("Enter the number of stacks you have:\n> "))
-  output = numToConv * 64
-  print(f"You have {output} blocks")
+	numToConv = int(input("Enter the number of stacks you have:\n> "))
+	output = numToConv * 64
+	print(f"You have {output} blocks")
 if stackOption == 2:
-  numToConv = int(input("Enter the number of blocks you have:\n> "))
-  output = numToConv // 64
-  remainder = numToConv % 64
-  print(f"You have {output} stacks and {remainder} blocks")
+	numToConv = int(input("Enter the number of blocks you have:\n> "))
+	output = numToConv // 64
+	remainder = numToConv % 64
+	print(f"You have {output} stacks and {remainder} blocks")

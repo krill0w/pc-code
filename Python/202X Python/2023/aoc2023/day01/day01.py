0330e3c257b3714d8d8a46da6e4c6d900dfb85f2
Dom Lowe <dom@kalowe.me.uk>
2023-12-19 13:07:50 +0000

    i did smth

--- Python/202X Python/2023/aoc2023/day01/day01.py
+++ Python/202X Python/2023/aoc2023/day01/day01.py
@@ -1,2 +1,22 @@
+import string
+
+lowerLetters = list(string.ascii_uppercase)
+runningTotal = int(0)
+formattedList = []
+
 with open("day01input.txt", "r") as f:
-    print(f.readlines())
+    lines = f.readlines()
+    print(lines)
+    for i in lines:
+        toString = lines[1]
+        lineAsStr = str(toString)
+        noLetters = lineAsStr.strip(lowerLetters)
+        noLetters = noLetters.strip("/")
+        formattedList.append(noLetters)
+
+    for line in noLetters:
+        singleNumbers = [int(num) for num in str(line)]
+        lineAns = sum(singleNumbers)
+        runningTotal += lineAns
+
+print(runningTotal)
